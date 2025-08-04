9e88f06ca94c6c885d4807930f69bbccd09da9af
RenanIurk <iurkrenan2@gmail.com>
2025-08-04 14:18:58 -0300

    feat: adicionado subtracao

--- src/Subtracao.java
+++ src/Subtracao.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +11,17 @@
  * @author info2
  */
 public class Subtracao {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int numero1 = scanner.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int numero2 = scanner.nextInt();
+
+        int resultado = numero1 - numero2;
+
+        System.out.println("A subtraçãos é: " + resultado);
+    }
 }
